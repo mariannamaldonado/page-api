d0fe0b4add0b508944a986372eb510148696d63c
mariannamaldonado <mariannamldndo@gmail.com>
2021-02-23 20:17:45 +0100

    update museum

--- src/components/Testapi.vue
+++ src/components/Testapi.vue
@@ -2,49 +2,43 @@
 <div class="row">
 <div class="prueba" >
     <h1>Agenda de actividades y eventos </h1>
-    <!-- //inicio container -->
-    
-  <div class="container">
-    <div class="row">
-      <div class="col">
-        <!-- inicio -->
-        <form class="d-flex">  
+   
+     <div class="container">
+       <div class="row">
+      <form class="d-flex">  
           <select class="form-select" v-model="event" @change="verSeleccionado">
             <option value="">--Selecciona una opción--</option>
             <option v-for="(event,i) in eventos" :key="i" :value="event['@id']"> {{event['title']}} </option>
           </select>
         </form>
+       </div>
+     </div>
+  
+  <div class="clearfix">
+    <img src="../assets/photo1.jpg" class="col-md-6 float-md-end mb-3 ms-md-3" alt="">
+  <div class="informacion" v-if="informacion.datos">
+  <h4 class="d-flex justify-content-center text-align-center"> {{informacion.datos.title}}:</h4>
+  <p style="text-align: left">
+    <span>Lugar: </span>{{informacion.datos['address']['locality']}} <br>
+  </p>
 
-        <div class="informacion" v-if="informacion.datos">
-          <div class="card text-center">
-            <div class="card-header">
-              <h4 class="d-flex justify-content-center text-align-center"> {{informacion.datos.title}}:</h4>
-            </div>
-            <div class="card-body">
-              <h5 class="card-title">
-                <span>Lugar: </span>{{informacion.datos['address']['locality']}} <br>
-              </h5>
-              <p class="card-text">
-                <span>Codigo postal: </span>{{informacion.datos['address']['postal-code']}} <br>.
-                <span>Dirección: </span>{{informacion.datos['address']['street-address']}} <br>
-                <span>Horario: </span>{{informacion.datos.organization['schedule']}} <br>
-                <span>Descripción: </span>{{informacion.datos.organization['organization-desc']}} <br>
-                <span>Servicios: </span>{{informacion.datos.organization['services']}}<br>
-              </p>
-              <a href="#" class="btn btn-primary">Ir al mapa</a>
-            </div>
-            <div class="card-footer text-muted">
-             2 days ago
-            </div>
-          </div>
-        </div>
-      </div>
-     <!-- fin -->
-    </div>
-  </div>
-
-     <!-- //fin container  -->
-
+  <p style="text-align: left">
+    <span>Codigo postal: </span>{{informacion.datos['address']['postal-code']}} <br>.
+  </p>
+  <p style="text-align: left">
+    <span style="text-align: left">Dirección: </span>{{informacion.datos['address']['street-address']}} <br>
+  </p>
+  <p style="text-align: left">
+    <span>Horario: </span>{{informacion.datos.organization['schedule']}} <br>
+  </p>
+  <p style="text-align: left">
+    <span >Descripción: </span>{{informacion.datos.organization['organization-desc']}} <br>
+  </p>
+  <p style="text-align: left">
+     <span>Servicios: </span>{{informacion.datos.organization['services']}}<br>
+  </p>
+</div>
+</div>
 </div>
 </div>
 </template>
@@ -101,4 +95,5 @@ export default {
   display: flex;
   margin: 50px;
 }
+
 </style>
